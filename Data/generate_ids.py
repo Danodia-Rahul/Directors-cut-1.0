55417e41dc0c33f23c878f41b9e9e5f14db4d634
Danodia-Rahul <danodiarahul17@gmail.com>
2025-08-20 20:21:47 +0530

    updated Data

---
+++ Data/generate_ids.py
@@ -0,0 +1,19 @@
+import hashlib 
+import json
+
+with open('merged_output.json', 'r') as f_in:
+    documents = json.load(f_in)
+
+def generate_id(record):
+    text = f"{record['type']} | {record['term']}"
+    return hashlib.md5(text.encode()).hexdigest()
+
+processed_data = []
+for record in documents:
+    unique_id = generate_id(record)
+    record['id'] = unique_id
+    processed_data.append(record)
+
+with open('Data/data.json', 'w') as f_out:
+    json.dump(processed_data, f_out, indent=2)
+
